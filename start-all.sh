fba066baed7a15ce09a84d2e3a8b67bf372f19a1
Ayush S Pangaonkar <ayushspangaonkar0710@gmail.com>
2026-02-13 15:54:55 +0530

    Update start-all.sh

--- start-all.sh
+++ start-all.sh
@@ -3,34 +3,31 @@
 echo "Starting Reddit Automation Stack"
 
 # Start reddit automation browser
-google-chrome \
+chromium \
 --remote-debugging-port=9222 \
 --user-data-dir=$HOME/chrome-cdp \
 https://reddit.com &
 
 sleep 5
 
-gnome-terminal -- bash -c "
-cd ~/projects/autopilot/reddit-automation
-node autopilot.js
-exec bash
-"
+# Start reddit automation
+cd ~/autopilot-main/reddit-automation
+node autopilot.js &
 
 
 echo "Starting Knowledge Automation Stack"
 
 # Start knowledge automation browser
-google-chrome \
+chromium \
 --remote-debugging-port=9224 \
 --user-data-dir=$HOME/chrome-cdp-seo \
 https://reddit.com &
 
 sleep 5
 
-gnome-terminal -- bash -c "
-cd ~/projects/autopilot/knowledge-automation
-node autopilot.js
-exec bash
-"
+# Start knowledge automation
+cd ~/autopilot-main/knowledge-automation
+node autopilot.js &
 
-echo "All automations started"
+
+echo "All automations started successfully"
